2cb77fd72f4cd0fd7095cdf1a9bf08b3e7fb4a49
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2021-01-14 12:59:54 +0000

    Partially working with msqss

--- consumer.c
+++ consumer.c
@@ -1,32 +1,50 @@
 #include "lib.h"
 #include "buffer.h"
+#include "notification.h"
 
 // IPC Clear handler
 void clearIPC(int signum);
 
 int main(int argc, char const *argv[])
-{
+{   
+    // Bind signal handlers
     signal(SIGINT, clearIPC);
     signal(SIGSEGV, clearIPC);
-    printf("Consumer has started\n");
-
+    printf("Consumer has started with pid %d\n", getpid());
+    
+    // Init IPC
     initBuffer();
+    initNotificationQueue();
     int buf_sem_id = getBufSemId();
+
+    int x;  // Carrys read value 
+    bool wasFull = false;
 
     while (1)
     {
         down(buf_sem_id);
-        if (!isBufEmpty()) {
-            int x = consume();
-            printf("I'm consuming %d\n", x);
-        } else {
-            printf("Buffer is empty\n");
+        if (isBufEmpty()) {
+            printf("Waiting for producer\n");
+            up(buf_sem_id);
+            waitForProducer();
+            down(buf_sem_id);
+            printf("Producer is here ! Consuming ...\n");
         }
+
+        // Consume a value
+        wasFull = isBufFull();
+        x = consume();
+        if (x != -1) printf("I'm consuming %d\n", x);
+
+        // Release and notify if full
+        if (wasFull) notifyProducer();
         up(buf_sem_id);
-        sleep(2);
+        sleep(1);
     }
 }
 
 void clearIPC(int signum) {
     destroyBuf();
+    destroyNotificationQueue();
+    exit((signum == SIGINT || signum == SIGSEGV) ? -1 : 0);
 }
--- producer.c
+++ producer.c
@@ -1,37 +1,49 @@
 #include "lib.h"
 #include "buffer.h"
+#include "notification.h"
 
 // IPC Clear handler
 void clearIPC(int signum);
-
 
 int main(int argc, char const *argv[])
 {
     signal(SIGINT, clearIPC);
     signal(SIGSEGV, clearIPC);
-    printf("Hi. I'm the producer\n");
+    printf("Hi. I'm the producer %d\n", getpid());
 
     // Init IPC
     initBuffer();
     int buf_sem_id = getBufSemId();
-
-    int val;
+    initNotificationQueue();
     
+    int val;    // Carrys produced value    
+    bool wasEmpty = false;
+
     while (1) {
         down(buf_sem_id);
-        if (!isBufFull()) {
-            val = rand() % 10000;
-            printf("I'm producing %d\n", val);
-            produce(val);
-        } else {
-            printf("Buffer is full !!\n");
+        if (isBufFull()) {
+            // Just wait for consumer notification
+            printf("Waiting for consumer\n");
+            up(buf_sem_id);
+            waitForConsumer();
+            down(buf_sem_id);
+            printf("Consumer is here ! Producing ...\n");
         }
+        // produce random value
+        val = rand() % 10000;
+        printf("I'm producing %d\n", val);
+        wasEmpty = isBufEmpty();
+        produce(val);
+
+        // Release and notify the consumer if empty
+        if (wasEmpty) notifyConsumer();
         up(buf_sem_id);
-        sleep(1);
+        sleep(2);
     }
 }
 
 void clearIPC(int signum) {
     destroyBuf();
-    exit(0);    
+    destroyNotificationQueue();
+    exit((signum == SIGINT || signum == SIGSEGV) ? -1 : 0);
 }

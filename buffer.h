538baf958201108b759a99557b82789975ae6de0
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2021-01-14 12:21:38 +0000

    Add more safety to buffer and init semaphore

--- buffer.h
+++ buffer.h
@@ -29,8 +29,10 @@ void initBuffer() {
         buf_addr->rd_idx = 0;
         buf_addr->wr_idx = 0;
         buf_addr->count = 0;
-        printf("I'm the first one to init the buffer. Kindly enter the size\n");
-        scanf("%d", &buf_addr->max_size);        
+        buf_addr->max_size = BUF_SIZE;
+
+        // Init buffer semaphore
+        initSem(buf_sem_id, 1);
     }
 }
 
@@ -43,31 +45,46 @@ void destroyBuf() {
     releaseShmAddr(buf_addr);
     printf("I'm detaching\n");
 
+    // Release the sem if I'm holding it
+    if (getSemPID(buf_sem_id) == getpid()) {
+        printf("I'm the one holding the semaphore so release !\n");
+        up(buf_sem_id);
+    }
+
     // Remove if last process attached
     if (getShmAttachesCount(buf_id) == 0) {
-        printf("I'm the last so I'll delete\n");
+        printf("I'm the last user of buf_shm so I'll delete\n");
         deleteShm(buf_id);
         deleteSemSet(buf_sem_id);
     }
 }
 
 void produce(int data) {
-    buf_addr->storage[buf_addr->wr_idx] = data;
-    buf_addr->wr_idx = (buf_addr->wr_idx + 1) % buf_addr->max_size;
-    buf_addr->count++;
+    if (buf_addr->count < buf_addr->max_size) {
+        buf_addr->storage[buf_addr->wr_idx] = data;
+        buf_addr->wr_idx = (buf_addr->wr_idx + 1) % buf_addr->max_size;
+        buf_addr->count++;
+    } else {
+        printf("Maaan :( I'll not produce if the buffer is full\n");
+    }
 }
 
 int consume() {
-    int data = buf_addr->storage[buf_addr->rd_idx];
-    buf_addr->rd_idx = (buf_addr->rd_idx + 1) % buf_addr->max_size;
-    buf_addr->count--;
-    return data;
+    if (buf_addr->count > 0) {
+        int data = buf_addr->storage[buf_addr->rd_idx];
+        buf_addr->rd_idx = (buf_addr->rd_idx + 1) % buf_addr->max_size;
+        buf_addr->count--;
+        return data;
+    } else {
+        printf("Maaan :( I'll not consume if the buffer is empty\n");
+        return -1;
+    }
 }
 
 bool isBufEmpty() {
-    return buf_addr->count == 0;
+    return buf_addr->count <= 0;
 }
 
 bool isBufFull() {
-    return buf_addr->count == buf_addr->max_size;
+    return buf_addr->count >= buf_addr->max_size;
 }
